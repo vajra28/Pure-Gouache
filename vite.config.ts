bc616a20b10ab5bf690e43b20b8c6adc2b361bed
vajra28 <49525432+vajra28@users.noreply.github.com>
2026-01-11 21:32:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: './', // Ensures assets load correctly on GitHub Pages
+  base: '/pure-gouache/', // Ensures assets load correctly on GitHub Pages
   plugins: [
     react(),
     VitePWA({
