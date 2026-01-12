1f22b321f525cfe8075dc4c3c6d6216b051c147e
vajra28 <pvajra@gmail.com>
2026-01-12 02:57:46 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Pure-Gouache/sw.js', { scope: '/Pure-Gouache/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
